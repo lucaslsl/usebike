c602c368a417a5de3919562210cf0bbafbce3948
Lucas Lima <lucas.lsl.lima@gmail.com>
2017-02-27 19:38:43 -0300

    Configuring angular template.

--- admin/js/routes.js
+++ admin/js/routes.js
@@ -4,22 +4,43 @@
  * Route configuration for the RDash module.
  */
 angular.module('RDash').config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-    function($stateProvider, $urlRouterProvider, $locationProvider) {
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-        // For unmatched routes
-        $urlRouterProvider.otherwise('/');
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
 
-        // Application routes
-        $stateProvider
-            .state('index', {
-                url: '/',
-                templateUrl: 'templates/dashboard.html'
-            })
-            .state('tables', {
-                url: '/tables',
-                templateUrl: 'templates/tables.html'
-            });
+    // Application routes
+    $stateProvider
+      .state('home', {
+          url: '/',
+          views: {
+            'main': {
+              templateUrl: 'templates/main.html',
+              controller: 'MasterCtrl'
+            }
+          }
+      })
+      .state('login', {
+          url: '/login',
+          views: {
+            'main': {
+              templateUrl: 'templates/login.html'
+            }
+          }
+      })
+      .state('tables', {
+          url: '/tables',
+          views: {
+            'main': {
+              templateUrl: 'templates/main.html',
+              controller: 'MasterCtrl'
+            },
+            'content': {
+              templateUrl: 'templates/tables.html'
+            }
+          }
+      });
 
-        $locationProvider.html5Mode(true);
-    }
+    $locationProvider.html5Mode(true);
+  }
 ]);
