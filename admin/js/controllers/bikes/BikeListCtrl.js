f3bdc65659d68e1acea1924db8ee4f8a2d8ff5be
Lucas Lima <lucas.lsl.lima@gmail.com>
2017-03-04 20:31:29 -0300

    angular login.

---
+++ admin/js/controllers/bikes/BikeListCtrl.js
@@ -0,0 +1,105 @@
+(function() {
+
+  angular
+    .module('UseBike')
+    .controller('BikeListCtrl', ['$http','$rootScope','$scope','toastr','$uibModal', BikeListCtrl]);
+
+  function BikeListCtrl($http, $rootScope, $scope, toastr,$uibModal) {
+
+    $scope.loadingBikes = true;
+
+    $scope.bikes = [];
+
+    $scope.pagination = {
+      currentPage: 1,
+      totalCount: 0,
+      perPage: 8,
+      totalCount: 0,
+      maxSize: 5
+    };
+
+    var listBikes = function(page){
+
+      $http({
+        method: 'GET',
+        data: "",
+        url:'/api/bikes/count'
+      })
+      .then(function(response){
+        $scope.pagination.totalCount = parseInt(response.data.total);
+        $http({
+          method: 'GET',
+          data: "",
+          url:'/api/bikes',
+          params: {limit: $scope.pagination.perPage, skip: (parseInt(page)-1) * $scope.pagination.perPage}
+        })
+        .then(function(response){
+          $scope.bikes = response.data;
+          $scope.loadingBikes = false;
+        });
+
+      });
+
+    }
+
+    listBikes(1);
+
+    $scope.pageChanged = function(){
+      listBikes($scope.pagination.currentPage);
+    }
+
+
+    $scope.delete = function(vehicle){
+      $rootScope.modal = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/util/delete_modal.html',
+        controller: 'VehicleDeleteCtrl',
+        size: 'md',
+        resolve: {
+          vehicle: function(){
+            return vehicle;
+          },
+          refresher: function(){
+            return $scope.pageChanged;
+          }
+        }
+      });
+    }
+
+    $scope.create = function(){
+      $rootScope.modal = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/catalog/vehicle/create_update.html',
+        controller: 'VehicleCreateUpdateCtrl',
+        size: 'md',
+        resolve: {
+          vehicle: function(){
+            return undefined;
+          },
+          refresher: function(){
+            return $scope.pageChanged;
+          }
+        }
+      });
+    }
+
+    $scope.edit = function(vehicle){
+      $rootScope.modal = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/catalog/vehicle/create_update.html',
+        controller: 'VehicleCreateUpdateCtrl',
+        size: 'md',
+        resolve: {
+          vehicle: function(){
+            return vehicle;
+          },
+          refresher: function(){
+            return $scope.pageChanged;
+          }
+        }
+      });
+    }
+
+  }
+
+})();
--- admin/js/controllers/master-ctrl.js
+++ admin/js/controllers/master-ctrl.js
@@ -3,37 +3,59 @@
  */
 
 angular.module('UseBike')
-    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+  .controller('MasterCtrl', ['$scope', '$cookieStore', '$rootScope', '$http', '$state', MasterCtrl]);
 
-function MasterCtrl($scope, $cookieStore) {
-    /**
-     * Sidebar Toggle & Cookie Control
-     */
-    var mobileView = 992;
+function MasterCtrl($scope, $cookieStore, $rootScope, $http, $state) {
+  /**
+   * Sidebar Toggle & Cookie Control
+   */
+  var mobileView = 992;
 
-    $scope.getWidth = function() {
-        return window.innerWidth;
-    };
+  $scope.getWidth = function() {
+    return window.innerWidth;
+  };
 
-    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        if (newValue >= mobileView) {
-            if (angular.isDefined($cookieStore.get('toggle'))) {
-                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
-            } else {
-                $scope.toggle = true;
-            }
-        } else {
-            $scope.toggle = false;
-        }
+  $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+    if (newValue >= mobileView) {
+      if (angular.isDefined($cookieStore.get('toggle'))) {
+        $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
+      } else {
+        $scope.toggle = true;
+      }
+    } else {
+      $scope.toggle = false;
+    }
 
+  });
+
+  $scope.toggleSidebar = function() {
+    $scope.toggle = !$scope.toggle;
+    $cookieStore.put('toggle', $scope.toggle);
+  };
+
+  window.onresize = function() {
+    $scope.$apply();
+  };
+
+  $scope.user = $rootScope.user;
+
+  console.log($rootScope.user);
+
+  $scope.logout = function(){
+    $http({
+      method: 'PUT',
+      data: '',
+      url:'/api/me/logout'
+    })
+    .then(function(response){
+      $rootScope.user = {};
+      $state.go('login');
+    })
+    .catch(function(response){
+      toastr.error('Um erro ocorreu. Tente novamente.');
     });
+  }
 
-    $scope.toggleSidebar = function() {
-        $scope.toggle = !$scope.toggle;
-        $cookieStore.put('toggle', $scope.toggle);
-    };
 
-    window.onresize = function() {
-        $scope.$apply();
-    };
+
 }
